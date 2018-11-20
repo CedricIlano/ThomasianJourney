6bdb007653ea8a225c72bfcd7b14736a4f1f4085
CedricIlano <2015081167@ust-ics.mygbiz.com>
2018-11-20 21:46:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/cedri_000/thomasianjourney/SplashActivity.java
@@ -0,0 +1,65 @@
+package com.example.cedri_000.thomasianjourney;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashActivity extends Activity{
+
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+        final TextView splash_text = (TextView) findViewById(R.id.splash_text);
+        final ImageView splash_image = (ImageView) findViewById(R.id.splash_image);
+        final Animation animation_1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate);
+        final Animation animation_2 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
+
+        animation_1.reset();
+        animation_2.reset();
+
+        splash_image.clearAnimation();
+        splash_image.startAnimation(animation_1);
+
+        splash_text.clearAnimation();
+        splash_text.startAnimation(animation_2);
+
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    while (waited < 1000) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    SplashActivity.this.finish();
+                } catch (InterruptedException e) {
+
+                } finally {
+                    SplashActivity.this.finish();
+                }
+
+            }
+        };
+        thread.start();
+    }
+}
+
